b9a2bea512418800743667d0822c57b1f940db1c
Peter Lundgren <peter@peterlundgren.com>
2013-06-28 14:57:32 -0400

    bowling: Add C solution

---
+++ bowling/c/bowling_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+assert () {
+    if [ ! $1 ]; then
+        echo >&2 "Assertion failed: \"$1\""
+        echo >&2 "File \"$0\", line $2"
+        exit 1
+    fi
+}
+
+assert "`./bowling XXXXXXXXXXXX` -eq 300" $LINENO
+assert "`./bowling 9-9-9-9-9-9-9-9-9-9-` -eq 90" $LINENO
+assert "`./bowling 5/5/5/5/5/5/5/5/5/5/5` -eq 150" $LINENO
